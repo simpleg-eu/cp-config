487432d3f9f5c9d8e7533a3e628e2a82d5e5fec9
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-12-14 02:22:32 +0100

    d package & extension

--- src/packager.rs
+++ src/packager.rs
@@ -6,6 +6,19 @@ use cp_core::error::Error;
 use std::path::Path;
 
 pub trait Packager {
+    ///
+    /// Packages the configuration files for the specified environment and component into the target package file.
+    ///
+    /// # Arguments
+    ///
+    /// * `environment` - Environment of configuration files to be used, i.e. development, staging, production.
+    /// * `component` - Configuration component, microservice, whose configuration files will be packaged into the
+    /// target package file.
+    /// * `target_file` - Package file.
     fn package(&self, environment: &str, component: &str, target_file: &Path) -> Result<(), Error>;
+
+    ///
+    /// Retrieves the extension of the resulting package file.
+    ///
     fn extension(&self) -> String;
 }
