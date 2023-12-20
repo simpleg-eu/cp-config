9a4c141e60ec66ed36970544e2e84dc6b3309034
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-12-20 01:12:51 +0100

    f warning of unused code

--- build.rs
+++ build.rs
@@ -4,15 +4,8 @@
 use std::path::Path;
 
 fn main() {
-    let build_mode = match option_env!("CARGO_CFG_DEBUG").map_or(false, |s| s == "true") {
-        true => "debug",
-        false => "release",
-    };
-
-    // Path to the file you want to copy
     let source_path = Path::new("config.yaml");
 
-    // Destination path in the build directory
     let dest_path = Path::new(&std::env::var("OUT_DIR").unwrap())
         .parent()
         .unwrap()
@@ -22,7 +15,6 @@ fn main() {
         .unwrap()
         .join("config.yaml");
 
-    // Attempt to copy the file
     if let Err(err) = std::fs::copy(&source_path, &dest_path) {
         eprintln!("error copying file: {}", err);
     } else {
